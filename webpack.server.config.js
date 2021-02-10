4202271799a8225c0e0f764d06bd2ced6312ed4c
Yevhenii Korchak <yevhenii@unostella.com>
2021-02-10 11:52:55 +0100

    Added configurations for ts, webpack, git, phpstorm

---
+++ webpack.server.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const minimist = require("minimist");
+const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const argv = minimist(process.argv);
+
+const mode = argv.mode || "development";
+
+/** Server Config */
+module.exports = {
+	mode: mode,
+	target: "node",
+	devtool: "source-map",
+	entry: {
+		"server": path.join(__dirname, `src/server/server.ts`),
+	},
+	output: {
+		path: path.join(__dirname, `dist/server`),
+		filename: `[name].js`,
+		chunkFilename: `[name].js`,
+	},
+	stats: {
+		preset: "errors-warnings",
+		colors: true,
+	},
+	performance: {
+		maxEntrypointSize: 10240000,
+		maxAssetSize: 5120000
+	},
+	externals: [nodeExternals()],
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'ts-loader',
+					options: {
+						configFile: path.join(__dirname, `tsconfig.json`),
+					}
+				}
+			},
+		]
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.join(__dirname, `src/server/modules/posts/logos`),
+					to: path.join(__dirname, `dist/server/logos`),
+				},
+			]
+		}),
+	],
+};
---
+++ webpack.web.config.js
@@ -0,0 +1,139 @@
+const path = require('path');
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const minimist = require("minimist");
+
+const argv = minimist(process.argv);
+
+const mode = argv.mode || "development";
+const port = argv.port || 9191;
+
+const sourceDir = path.join(__dirname, `src/web`);
+const distDir = path.join(__dirname, `dist/web`);
+
+/** Web Config */
+module.exports = {
+	mode: mode,
+	target: "web",
+	devtool: mode === "development" ? "eval-source-map" : "source-map",
+	entry: {
+		"index": path.join(sourceDir, `index.tsx`),
+	},
+	output: {
+		path: distDir,
+		filename: `js/[name].js`,
+		chunkFilename: `js/[name].js`,
+		publicPath: `/assets/`
+	},
+	devServer: {
+		contentBase: sourceDir,
+		compress: true,
+		port: port,
+		historyApiFallback: {
+			rewrites: [
+				{
+					from: /^.*$/,
+					to: () => {
+						return "index.html";
+					}
+				}
+			]
+		},
+		stats: {
+			preset: "errors-warnings",
+			colors: true,
+			timings: true,
+		}
+	},
+	stats: {
+		preset: "errors-warnings",
+		colors: true,
+	},
+	performance: {
+		maxEntrypointSize: 1024000,
+		maxAssetSize: 512000
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'ts-loader',
+					options: {
+						configFile: path.join(__dirname, `tsconfig.json`),
+					}
+				}
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "file-loader",
+					options: {
+						name: "img/[name].[ext]?h=[contenthash]",
+					}
+				}
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+					mode === "development" ? {
+						loader: 'style-loader',
+						options: {}
+					} : {
+						loader: MiniCssExtractPlugin.loader,
+						options: {}
+					},
+					{
+						loader: 'css-loader',
+						options: {}
+					},
+					{
+						loader: 'resolve-url-loader',
+						options: {}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+						}
+					},
+					{
+						loader: 'sass-resources-loader',
+						options: {
+							resources: [
+								path.join(sourceDir, `partials.scss`),
+							]
+						},
+					},
+				],
+			},
+		]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			"process.env": {
+				// 	NODE_ENV: JSON.stringify(ENV),
+				// 	// MODE: JSON.stringify(MODE),
+				mode: JSON.stringify(mode),
+			}
+		}),
+		new MiniCssExtractPlugin({
+			filename: "css/index.css",
+		}),
+		// new OptimizeCssAssetsPlugin(),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.join(sourceDir, `index.html`),
+					to: path.join(distDir, `index.html`)
+				},
+			]
+		}),
+	],
+};
