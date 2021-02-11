6274447cb8373a5dfaf200c7da5ec04b24c6c3e3
yevheni <yevhenii.korchak@gmail.com>
2021-02-11 23:26:50 +0100

    web: calendar - updated layout

--- src/web/app/calendar/calendar.ts
+++ src/web/app/calendar/calendar.ts
@@ -8,38 +8,39 @@ export const Calendar = Base.extend({
 
 	data() {
 		return {
-			year: moment().year(),
+			date: moment(),
 			weekdays: Array(7).fill(null).map((el, i) => {
 				return {
 					text: moment().weekday(i + 1).format("dddd"),
 				}
 			}),
-			get months() {
-				return Array(12).fill(null).map((el, i) => {
-					const month = moment(this.year).month(i);
-
-					return {
-						index: i,
-						name: month.format("MMMM"),
-						days: Array(month.daysInMonth()).fill(null).map((el, i) => {
-							const day = moment(month).date(i + 1);
-
-							return {
-								text: day.format("DD"),
-								day_of_week: day.weekday(),
-								day: day,
-							}
-						}),
-						month: month,
-					};
-				})
-			},
+			// get months() {
+			// 	return Array(12).fill(null).map((el, i) => {
+			// 		const month = moment(this.year).month(i);
+			//
+			// 		return {
+			// 			index: i,
+			// 			name: month.format("MMMM"),
+			// 			days: Array(month.daysInMonth()).fill(null).map((el, i) => {
+			// 				const day = moment(month).date(i + 1);
+			//
+			// 				return {
+			// 					text: day.format("DD"),
+			// 					day_of_week: day.weekday(),
+			// 					day: day,
+			// 				}
+			// 			}),
+			// 			month: month,
+			// 		};
+			// 	})
+			// },
 		}
 	},
 
-	methods: {},
-
-	created() {
-		console.log("months:", this.months)
-	}
+	methods: {
+		updateMonth(inc: number) {
+			this.date.add(inc, "month");
+			this.$forceUpdate();
+		}
+	},
 });
