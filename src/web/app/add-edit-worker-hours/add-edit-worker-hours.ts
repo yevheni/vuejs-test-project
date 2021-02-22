237baff9e65213799753864d3faba8c0dff75338
yevheni <yevhenii.korchak@gmail.com>
2021-02-22 23:37:06 +0100

    web: add-edit-worker-hours - updated layout

--- src/web/app/add-edit-worker-hours/add-edit-worker-hours.ts
+++ src/web/app/add-edit-worker-hours/add-edit-worker-hours.ts
@@ -5,74 +5,80 @@ import {IWorker} from "../../interfaces/worker";
 export const AddEditWorkerHours = Base.extend({
 	template: template,
 
-	props: [
-		"date",
-		"hours",
-	],
+	props: {
+		dates: {
+			type: Array,
+			required: true,
+			default: [],
+		}
+	},
 
 	data() {
 		return {
+			// date: moment(),
 			workers: this.$store.state.workers,
-			workerId: this.hours?.worker || "",
-			start: this.hours?.start || "",
-			end: this.hours?.end || ""
+			// workerId: this.hours?.worker || "",
+			// start: this.hours?.start || "",
+			// end: this.hours?.end || ""
+			form: {
+				worker: "",
+				date: "",
+				start: "",
+				end: "",
+			}
 		}
 	},
 
 	methods: {
-		getWorker(id: string) {
-			return this.workers.find((w: IWorker) => w._id === id);
-		},
+		// getWorker(id: string) {
+		// 	return this.workers.find((w: IWorker) => w._id === id);
+		// },
 
 		async onSubmit(e: Event) {
 			try {
-				if (!this.workerId) throw new Error(`Worker not selected`);
-				if (this.start === "") throw new Error(`Start hours not selected`);
-				if (this.end === "") throw new Error(`End hours not selected`);
-				if (this.start >= this.end) throw new Error(`Start hour can't be greater or equal than end hour`);
-				if (this.end <= this.start) throw new Error(`End hour can't be lower or equal than start hour`);
+				if (!this.form.worker) throw new Error(`Worker not selected`);
+				if (!this.form.date) throw new Error(`Day not selected`);
+				if (this.form.start === "") throw new Error(`Start hours not selected`);
+				if (this.form.end === "") throw new Error(`End hours not selected`);
+				if (this.form.start >= this.form.end) throw new Error(`Start hour can't be greater or equal than end hour`);
+				if (this.form.end <= this.form.start) throw new Error(`End hour can't be lower or equal than start hour`);
 
-				if (this.hours) {
-					/** Update */
-					const hoursRes = await this.api.put("/hours", {
-						id: this.hours._id,
-						start: this.start,
-						end: this.end,
-						date: this.date.valueOf(),
-					});
-					const updatedHours = hoursRes.data.hours;
-
-					this.$emit("done", updatedHours);
-				} else {
+				// if (this.hours) {
+				// 	/** Update */
+				// 	const hoursRes = await this.api.put("/hours", {
+				// 		id: this.hours._id,
+				// 		start: this.start,
+				// 		end: this.end,
+				// 		date: this.date.valueOf(),
+				// 	});
+				// 	const updatedHours = hoursRes.data.hours;
+				//
+				// 	this.$emit("done", updatedHours);
+				// } else {
 					/** Create */
-					const hoursRes = await this.api.post("/hours", {
-						worker: this.workerId,
-						start: this.start,
-						end: this.end,
-						date: this.date.valueOf(),
-					});
+					const hoursRes = await this.api.post("/hours", this.form);
 					const createdHours = hoursRes.data.hours;
 
 					this.$emit("done", createdHours);
-				}
+				// }
 			} catch (err) {
 				this.errorHandle(err);
 			}
 		},
 
-		async deleteHours() {
-			try {
-				await this.api.delete("/hours", {
-					params: {
-						id: this.hours._id,
-					}
-				});
-
-				this.$emit("deleted");
-			} catch(err) {
-				this.errorHandle(err);
-			}
-		}
+		// async deleteHours() {
+		// 	try {
+		// 		await this.api.delete("/hours", {
+		// 			params: {
+		// 				id: this.hours._id,
+		// 			}
+		// 		});
+		//
+		// 		this.$emit("deleted");
+		// 	} catch(err) {
+		// 		this.errorHandle(err);
+		// 	}
+		// }
 	},
 
 
