acf1036d4496cea3edcacafab42883db851d53e3
Yevhenii Korchak <yevhenii@unostella.com>
2021-02-23 11:41:37 +0100

    web: add-edit-worker-hours - updated layout and functionality

--- src/web/app/add-edit-worker-hours/add-edit-worker-hours.ts
+++ src/web/app/add-edit-worker-hours/add-edit-worker-hours.ts
@@ -1,6 +1,5 @@
 import template from "./add-edit-worker-hours.html";
 import {Base} from "../../helpers/base";
-import {IWorker} from "../../interfaces/worker";
 import moment from "moment";
 
 export const AddEditWorkerHours = Base.extend({
@@ -10,51 +9,54 @@ export const AddEditWorkerHours = Base.extend({
 		date: {
 			required: true,
 			default: moment(),
-		}
+		},
+		id: {
+			type: String,
+			default: "",
+		},
+		worker: {
+			type: String,
+			default: "",
+		},
+		start: {
+			default: "",
+		},
+		end: {
+			default: "",
+		},
 	},
 
 	data() {
 		return {
-			// date: moment(),
 			workers: this.$store.state.workers,
-			// workerId: this.hours?.worker || "",
-			// start: this.hours?.start || "",
-			// end: this.hours?.end || ""
 			form: {
-				worker: "",
-				// date: "",
-				start: "",
-				end: "",
+				worker: this.$props.worker,
+				start: this.$props.start,
+				end: this.$props.end,
 			}
 		}
 	},
 
 	methods: {
-		// getWorker(id: string) {
-		// 	return this.workers.find((w: IWorker) => w._id === id);
-		// },
-
 		async onSubmit(e: Event) {
 			try {
 				if (!this.form.worker) throw new Error(`Worker not selected`);
-				// if (!this.form.date) throw new Error(`Day not selected`);
 				if (this.form.start === "") throw new Error(`Start hours not selected`);
 				if (this.form.end === "") throw new Error(`End hours not selected`);
 				if (this.form.start >= this.form.end) throw new Error(`Start hour can't be greater or equal than end hour`);
 				if (this.form.end <= this.form.start) throw new Error(`End hour can't be lower or equal than start hour`);
 
-				// if (this.hours) {
-				// 	/** Update */
-				// 	const hoursRes = await this.api.put("/hours", {
-				// 		id: this.hours._id,
-				// 		start: this.start,
-				// 		end: this.end,
-				// 		date: this.date.valueOf(),
-				// 	});
-				// 	const updatedHours = hoursRes.data.hours;
-				//
-				// 	this.$emit("done", updatedHours);
-				// } else {
+				if (!!this.id) {
+					/** Update */
+					const hoursRes = await this.api.put("/hours", {
+						id: this.id,
+						start: this.form.start,
+						end: this.form.end,
+					});
+					const updatedHours = hoursRes.data.hours;
+
+					this.$emit("done", updatedHours);
+				} else {
 					/** Create */
 					const hoursRes = await this.api.post("/hours", {
 						...this.form,
@@ -63,26 +65,24 @@ export const AddEditWorkerHours = Base.extend({
 					const createdHours = hoursRes.data.hours;
 
 					this.$emit("done", createdHours);
-				// }
+				}
 			} catch (err) {
 				this.errorHandle(err);
 			}
 		},
 
-		// async deleteHours() {
-		// 	try {
-		// 		await this.api.delete("/hours", {
-		// 			params: {
-		// 				id: this.hours._id,
-		// 			}
-		// 		});
-		//
-		// 		this.$emit("deleted");
-		// 	} catch(err) {
-		// 		this.errorHandle(err);
-		// 	}
-		// }
+		async deleteHours() {
+			try {
+				await this.api.delete("/hours", {
+					params: {
+						id: this.id,
+					}
+				});
+
+				this.$emit("deleted");
+			} catch(err) {
+				this.errorHandle(err);
+			}
+		}
 	},
-
-
 });
